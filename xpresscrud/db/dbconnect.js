30360bce956495bbc10afd569a5aea646f7eed18
Somesh Dattatraya Dahotre <131247687+totally-somesh@users.noreply.github.com>
2023-06-28 08:21:33 +0530

    Xpress CRUD
    ~ This project provides connection to DB.

---
+++ xpresscrud/db/dbconnect.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+
+//create connection
+var mysqlconnection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "JoeRoot123",
+  database: "test",
+  port: 3306,
+});
+mysqlconnection.connect((err) => {
+  if (err) {
+    console.log("connection failed" + JSON.stringify(err));
+  } else {
+    console.log("connection done successfully");
+  }
+});
+
+module.exports = mysqlconnection;
